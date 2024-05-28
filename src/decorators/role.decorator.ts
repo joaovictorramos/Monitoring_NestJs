a41d0fcaea0a39146f6f42d8ee4221cbf09f5ba9
joaovictorramos <joaoramosreis24@gmail.com>
2024-05-28 06:30:51 -0300

    Update Roles key

--- src/decorators/role.decorator.ts
+++ src/decorators/role.decorator.ts
@@ -1,5 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from 'src/enums/role.enum';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-export const ROLES_KEY = 'roles';
+export const ROLES_KEY = process.env.ROLES_KEY;
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
