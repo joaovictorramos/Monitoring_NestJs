0b728bcdc66e1cc8985d0281e12a2b206f81a0d5
joaovictorramos <joaoramosreis24@gmail.com>
2024-06-03 23:40:25 -0300

    Adding CORS and update findAll of Monitor class

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { TimeoutInterceptor } from './interceptor/timeout.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
